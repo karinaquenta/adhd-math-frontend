be981c2a7c7c433729629524022ad84acac3b9eb
Karina <karinaxquenta@gmail.com>
2024-07-15 10:28:36 -0700

    Added my created components to my intro page

--- app/onboarding/intro/page.tsx
+++ app/onboarding/intro/page.tsx
@@ -1,3 +1,12 @@
+import React from "react";
+import BackButton from "../components/intro-back-button";
+import SpeechBox from "../components/speech-box";
+
 export default function Page() {
-  return <h1>Hi! I am Digi</h1>;
+  return (
+  <div className= "flex  items-center flex-col h-screen">
+    <BackButton />
+    <SpeechBox title="Hi! I am Digi, your math buddy!" text="To get started, let's create an account." />
+    </div>
+    );
 }
