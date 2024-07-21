2acd43431629152a5f0f1f0edbf6f473262bab65
Karina <karinaxquenta@gmail.com>
2024-07-21 14:13:18 -0700

    updated component to include image next to text and formatting

--- app/onboarding/components/speech-box.tsx
+++ app/onboarding/components/speech-box.tsx
@@ -4,20 +4,24 @@ import Check from "../../../public/onboarding/Icon/Vector.svg";
 
 
 
-export default function SpeechBox({ title, text }: { title: string; text: string }) {
+export default function SpeechBox({ title, text, imgSrc }: { title: string; text: string; imgSrc: string }) {
   return (
   
-    <div className="flex flex-col items-center">
-      <div className="bg-white p-6 rounded-tr-lg rounded-tl-lg rounded-br-lg border border-gray-300 mt-20 flex items-start">
-        <div className="text-gray-500 mr-4">&#x1F50A;</div>
+    <div className="flex flex-col items-center mt-20">
+      <div className="flex items-start">
+        <div className="mr-4">
+          <Image src={imgSrc} alt="monsterIcon" width={400} height={400} />
+        </div>
+        <div className="bg-white p-10 rounded-tr-lg rounded-tl-lg rounded-br-lg border-4 border-gray-300 flex items-start">
+        <div className="bg-gray-500 text-white font-bold w-10 h-10 flex  items-center justify-center rounded-full mr-4">
+          &#x1F50A;
+        </div>
         <div>
           <h2 className="text-xl font-bold mb-2">{title}</h2>
           <p>{text}</p>
         </div>
       </div>
-      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full mt-4 w-10 h-10">
-        &#x1F50A;
-      </button>
+      </div>
     </div>
   );
 }
