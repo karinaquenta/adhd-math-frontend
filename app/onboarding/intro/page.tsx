d85891efeab7fac8be7761ae96b1b133dd1c78f7
Karina <karinaxquenta@gmail.com>
2024-07-21 14:20:42 -0700

    updated intro page to include updated forward button component and speech box component

--- app/onboarding/intro/page.tsx
+++ app/onboarding/intro/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import BackButton from "../components/intro-back-button";
 import SpeechBox from "../components/speech-box";
+import ForwardButton from "../components/intro-forward-button";
+
 
 export default function Page() {
   return (
   <div className= "flex  items-center flex-col h-screen">
     <BackButton />
-    <SpeechBox title="Hi! I am Digi, your math buddy!" text="To get started, let's create an account." />
-    </div>
+    <SpeechBox 
+    title="Hi! I am Digi, your math buddy!" 
+    text="To get started, let's create an account." 
+    imgSrc="/onboarding/Icon/monster31.png"/>
+    <ForwardButton />
+  </div>
     );
 }
